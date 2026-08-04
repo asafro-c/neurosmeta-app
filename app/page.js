4cb7258f19226c96d93ebbfa79354a8617ba70dd
asafro-c <alekseisafro@gmail.com>
2026-08-04 11:35:17 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial", textAlign: "center" }}>
+      <h1>НейроСмета</h1>
+      <p>Приложение для подготовки сметной документации</p>
+      <p style={{ color: "#888", marginTop: 20 }}>Сервис запущен. API эндпоинты активны.</p>
+    </div>
+  );
+}
